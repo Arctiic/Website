bfffa9aac8f7aab2461d230a88c8e6bf314bf495
Brian Shao <brianshao0526@outlook.com>
2017-10-21 20:44:55 -0700

    Use Header HTML File

--- controller/controller.js
+++ controller/controller.js
@@ -1,24 +1,26 @@
 const path = require('path');
 const express = require('express');
 const log = require('logtools');
+const react = require('react');
+const reactDOM = require('react-dom');
 
 module.exports = (app, io) => {
 	//Main Homepage
 	app.get('/', (req, res) => {
-		res.sendFile(path.resolve(`${__dirname}/../cli/html/index.html`));
+		res.sendFile(path.resolve(`${__dirname}/../cli/html/home.html`));
 	});
 
 	//Code Page
 	app.get('/code', (req, res) => {
-		res.sendFile(path.resolve(`${__dirname}/../cli/html/index.html`));
+		res.sendFile(path.resolve(`${__dirname}/../cli/html/code.html`));
 	});
 
 	//URL Hub
 	app.get('/hub', (req, res) => {
-		res.sendFile(path.resolve(`${__dirname}/../cli/html/index.html`));
+		res.sendFile(path.resolve(`${__dirname}/../cli/html/hub.html`));
 	});
 
-	//Manual Error Handling
+	//Manual Testing
 	app.get('/err/:id', (req, res) => {
 		res.sendFile(path.resolve(`${__dirname}/../cli/errors/${req.params.id || "404"}.html`));
 	})
