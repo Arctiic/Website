f4561b6ea143c93754a6395e55a35bfa9d107c7c
Brian Shao <brianshao0526@outlook.com>
2017-10-28 08:55:55 -0700

    Fix package error with stringtables

--- controller/logger.js
+++ controller/logger.js
@@ -5,7 +5,7 @@ const st = require('stringtables');
 module.exports = (app, io, t) => {
 	app.use((req, res, next) => {
 		let log = t.newLine(
-			new Line(
+			new st.Line(
 				req.method || '',
 				req.originalUrl || '',
 				req.statusCode || '',
