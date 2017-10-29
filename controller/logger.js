07addbffac8010b9fc83678e5c8d03c4e3d29821
Brian Shao <brianshao0526@outlook.com>
2017-10-29 09:26:51 -0700

    Fix removed function

--- controller/logger.js
+++ controller/logger.js
@@ -6,11 +6,11 @@ module.exports = (app, io, t) => {
 	app.use((req, res, next) => {
 		let log = t.newLine(
 			new st.Line(
-				`${req.method}` || '',
-				`${req.originalUrl}` || '',
-				`${res.statusCode}  ` || '',
+				`${req.method}`,
+				`${req.originalUrl}`,
+				`${res.statusCode}  `,
 				" Latency ",
-				ip(req).clientIp || ''
+				`${req.ip}`
 			)
 		);
 		console.log(log);
