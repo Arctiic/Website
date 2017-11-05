c7f5d933402444dd13d4602e0535b9df197fdc69
Brian Shao <brianshao0526@outlook.com>
2017-11-05 08:39:10 -0800

    Add HNAP1 Exploit safteynet

--- controller/safteynet.js
+++ controller/safteynet.js
@@ -4,7 +4,16 @@ const log = require('logtools');
 
 module.exports = (app, io) => {
 	//PHPMyAdmin
-	app.get('/phpMyAdmin/*', (req, res) => {
+	app.all('/phpMyAdmin/*', (req, res) => {
+		res.send(`
+      <h1></h1>
+      <h3>Goodbye, Bot (or Hacker xP)</h3>
+    `);
+    res.status(418).end();
+	});
+
+	//HNAP1
+	app.all('/HNAP1', (req, res) => {
 		res.send(`
       <h1></h1>
       <h3>Goodbye, Bot (or Hacker xP)</h3>
