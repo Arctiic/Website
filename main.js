22c735887f50d265c7398323a4a4b07c8b452185
Brian Shao <brianshao0526@outlook.com>
2017-12-05 17:04:51 -0800

    Organiation Changes

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const st = require('stringtables');
 
 const t = new st.Table(" Type ", " Path                               ", " Status ", " Latency ", " IP                 ", " Port ");
 const controllers = [
-	'logger',
+	'dedicated',
 	'controller',
 	'errors'
 ];
@@ -19,6 +19,35 @@ app.use('/cli', express.static(`${__dirname}/cli/`));
 
 app.enable('case sensitive routing');
 app.enable('trust proxy');
+
+app.all('*', (req, res, next) => {
+	let method =
+		req.method;
+	let path =
+		req.originalUrl;
+	let status =
+		res.statusCode;
+	let ip =
+		req.headers['x-real-ip'] ||
+		req.connection.remoteAddress;
+	let port =
+		req.headers['x-forwarded-port'] ||
+		req.connection.remotePort;
+
+	let line = t.newLine(
+		new st.Line(
+			`${method}`,
+			`${path}`,
+			`${status}`,
+			"0 ms",
+			`${ip}`,
+			`${port}`
+		)
+	);
+
+	console.log(line);
+	next();
+});
 
 for (let i = 0; i < controllers.length; i++) {
 	require(`./controller/${controllers[i]}.js`)(app, io, t);
